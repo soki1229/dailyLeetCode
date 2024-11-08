36554c1e5712778aab9abf2f9c7f2a8291593062
soki <woosukplay3@gmail.com>
2024-11-09 00:05:01 +0900

    [2024.11.08] 1829. Maximum XOR for Each Query Solved
    - Let code be much faster, more efficient.

--- src/1829. Maximum XOR for Each Query Solved.cpp
+++ src/1829. Maximum XOR for Each Query Solved.cpp
@@ -4,16 +4,13 @@ class Solution {
 public:
     vector<int> getMaximumXor(vector<int>& nums, int maximumBit)
     {
-        vector<int> ret;
+        vector<int> ret(nums.size());
         
         int val = pow(2, maximumBit) - 1;
-        for (int i = 0; i < nums.size(); i++)
-        {
-            val ^= nums[i];
-            ret.push_back(val);
-        }
-
-        reverse(ret.begin(), ret.end());
+        
+        auto it = ret.rbegin();
+        for (auto n : nums)
+            *it++ = val ^= n;
         
         return ret;
     }
